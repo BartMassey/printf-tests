0411b412f3957ee7c6ca2f0abfe9aa579be655c5
Bart Massey <bart@cs.pdx.edu>
2013-08-05 15:19:18 -0700

    got gen-testcases to process the first 50 test cases

--- drivers/c/gen-testcases.hs
+++ drivers/c/gen-testcases.hs
@@ -3,37 +3,95 @@
 -- Please see the file COPYING in this distribution for
 -- license terms.
 
-import Control.Monad
-import Data.Int
-import Data.Word
-import System.IO
+import Data.Char
+import Data.List (find)
 import Text.Printf
 
 data Field = FieldString String | FieldChar Char | 
              FieldSigned Integer | FieldUnsigned Integer |
-             FieldPointer Integer
+             FieldPointer Integer | FieldFloating Double
+
+parseFields :: String -> [Field]
+parseFields "" = []
+parseFields (' ' : cs) = parseFields cs
+parseFields ('\'' : c : '\'' : cs) = FieldChar c : parseFields cs
+parseFields cs@(c : _) | isNumeric c =
+  let (s, cs') = span isNumeric cs in
+  case isIntegral s of
+    True ->
+      let i = case s of
+            "" -> error "stray minus sign in input"
+            _ -> read s :: Integer 
+      in
+       case i >= 0 of
+         True ->
+           case cs' of
+             'U' : 'L' : 'L' : cs'' -> FieldUnsigned i : parseFields cs''
+             'U' : cs'' -> FieldUnsigned i : parseFields cs''
+             'L' : 'L' : cs'' -> FieldSigned i : parseFields cs''
+             'V' : 'L' : 'L' : cs'' -> FieldPointer i : parseFields cs''
+             'V' : cs'' -> FieldPointer i : parseFields cs''
+             _ -> FieldSigned i : parseFields cs'
+         False ->
+           case cs' of
+             'U' : _ -> error "refused to parse negative unsigned value"
+             'L' : 'L' : cs'' -> FieldSigned (negate i) : parseFields cs''
+             'V' : _ -> error "refused to parse negative pointer value"
+             _ -> FieldSigned (negate i) : parseFields cs'
+    False ->
+      let d = case reads ('-' : s) :: [(Double, String)] of
+            [(d', "")] -> d'
+            _ -> error "badly formatted floating point number in input"
+      in
+       case cs' of
+         'F' : cs'' -> FieldFloating d : parseFields cs''
+         _ -> FieldFloating d : parseFields cs'
+  where
+    isNumeric c' = isDigit c' || c' `elem` "-+.eE"
+    isIntegral ('-' : cs') = all isDigit cs'
+    isIntegral cs' = all isDigit cs'
+parseFields ('"' : cs) =
+  let (s, cs') = span (/= '"') cs in
+  case cs' of
+    '"' : cs'' -> FieldString s : parseFields cs''
+    _ -> error "unclosed string"
+parseFields _ = error "stray character in input"
 
 instance Show Field where
   show (FieldString s) = show s
   show (FieldChar c) = show c
-  show (FieldSigned i) | i >= i <= 
+  show (FieldSigned i)
+    | i >= (-2147483648) && i <= 2147483647 = show i
+    | otherwise = show i ++ "LL"
+  show (FieldUnsigned u)
+    | u >= 0 && u <= 4294967295 = show u ++ "U"
+    | u >= 0 = show u ++ "ULL"
+    | otherwise = error "refused to show signed unsigned value"
+  show (FieldPointer u)
+    | u >= 0 && u <= 4294967295 = "(void *)" ++ show u ++ "U"
+    | u >= 0 = "(void *)" ++ show u ++ "ULL"
+    | otherwise = error "refused to show signed pointer value"
+  show (FieldFloating d) = show d
 
 genCase :: String -> IO ()
 genCase testcase =
-  let (serial : result : format : args) = parseFields testcase in
-  printf "    test(%d, %s, %s" (show serial) (show result) (show format)
-  mapM_ (printf ", %s" . show) args
-  printf ");\n"
+  case parseFields testcase of
+    serial : result : format : args -> do
+      _ <- printf "    test(%s, %s, %s" (show serial) (show result) (show format)
+      mapM_ (printf ", %s" . show) args
+      printf ");\n"
+    _ -> 
+      error $ printf "bad parse for: %s" testcase
 
 main :: IO ()
 main = do
   contents <- getContents
-  mapM_ genCase $ filter (nonblank && noncomment) $ unlines contents
+  mapM_ genCase $ filter (\s -> nonblank s && noncomment s) $ lines contents
   where
     nonblank s =
-      case find (\= ' ') s of
-        Nothing -> True
-        _ -> False
+      case find (/= ' ') s of
+        Nothing -> False
+        _ -> True
     noncomment [] = True
     noncomment ('#' : _) = False
     noncomment _ = True
